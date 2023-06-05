526fed275ae67cf2526fa8ef17d23e10a959640b
Vinicius Boscardin <stonervina@gmail.com>
2023-06-04 21:02:17 -0300

    commit

---
+++ src/App.styles.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const Container = styled.div``;
+
+export const Header = styled.div`
+  background-color: #8341a5;
+  height: 150px;
+  text-align: center;
+`;
+
+export const HeaderText = styled.h1`
+  font-family: 'Roboto', sans-serif;
+  margin: 0;
+  padding: 0;
+  color: #fff;
+  padding-top: 30px;
+`;
+
+export const Body = styled.div`
+  font-family: 'Roboto', sans-serif;
+  margin: auto;
+  max-width: 980px;
+  margin-bottom: 50px;
+`;
---
+++ src/data/items.ts
@@ -0,0 +1,28 @@
+import { Item } from '../types/Item';
+
+export const items: Item[] = [
+  {
+    date: new Date(2023, 5, 4),
+    category: 'food',
+    title: 'Mcdonalds',
+    value: 32.12,
+  },
+  {
+    date: new Date(2023, 5, 4),
+    category: 'food',
+    title: 'Burger King',
+    value: 28,
+  },
+  {
+    date: new Date(2023, 5, 5),
+    category: 'rent',
+    title: 'Aluguel Apt',
+    value: 2300,
+  },
+  {
+    date: new Date(2023, 6, 6),
+    category: 'Salary',
+    title: 'Salário',
+    value: 4000,
+  },
+];
